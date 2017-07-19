70b81a7cdbaca2f4bd5003953c2a0a4f52f30818
Mori <iwona.moryc92@gmail.com>
2017-07-19 17:40:08 +0200

    Player Ctrl & Animations

--- Assets/PlayerCtrl.cs
+++ Assets/PlayerCtrl.cs
@@ -10,12 +10,16 @@ public class PlayerCtrl : MonoBehaviour {
     /// 
 
     [Tooltip("This is a positive float that speeds up player movement")]
-    public float speedBoost;
+    public float speedBoost; // initialize 5
+    public float jumpSpeed; // initialize 600
+
     private Rigidbody2D rbd;
+    private SpriteRenderer sr;
        
 	void Start () {
 
         rbd = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
 	}
 	
 	void Update () {
@@ -27,15 +31,31 @@ public class PlayerCtrl : MonoBehaviour {
             MoveHorizontal(playerSpeed);
         else
             StopMoving();
+
+             
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+        
     }
 
     void MoveHorizontal(float playerSpeed)
     {
         rbd.velocity = new Vector2(playerSpeed, rbd.velocity.y);
+        if (playerSpeed > 0)
+            sr.flipX = false;
+        else if (playerSpeed < 0)
+            sr.flipX = true;
     }
 
     void StopMoving()
     {
         rbd.velocity = new Vector2(0, rbd.velocity.y);
     }
+
+    void Jump()
+    {
+        rbd.AddForce(new Vector2(0, jumpSpeed));
+    }
+
+   
 }
